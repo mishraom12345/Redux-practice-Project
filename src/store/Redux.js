a8432e5bcedbf1b8aee714f2c2e6930721e3abdd
mishraom12345 <mishrahariom525@gmail.com>
2023-02-28 09:50:31 +0530

    increment and decrement by 2

--- src/store/Redux.js
+++ src/store/Redux.js
@@ -3,11 +3,11 @@ import { createStore } from "redux";
 const counterReducer = (state={counter:0}, action) => {
     if(action.type === 'increment'){
         return{
-            counter:state.counter + 5
+            counter:state.counter + 2
         }
     }else if(action.type === 'decrement'){
         return{
-            counter:state.counter - 5
+            counter:state.counter - 2
         }
     }else{
         return state
