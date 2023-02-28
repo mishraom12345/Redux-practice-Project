a9db797de1e9be4ba6c6c1a8ef8b7ae668ee267d
mishraom12345 <mishrahariom525@gmail.com>
2023-02-28 18:07:31 +0530

    implement login and logout using redux tool kit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,9 +3,9 @@ import { counterActions } from '../store/Redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const counter = useSelector(state=>state.counter)
+  const counter = useSelector(state=>state.counter.counter)
   const dispatch = useDispatch()
-  const show = useSelector(state=>state.showcounter)
+  const show = useSelector(state=>state.counter.showcounter)
 
   const incrementhandler = ()=>{
     dispatch(counterActions.increment())
