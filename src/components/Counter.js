60b8fd2e560b46db9190eafa61aa8e50525b15dd
mishraom12345 <mishrahariom525@gmail.com>
2023-02-28 09:54:34 +0530

    making increament & decreament counter by 5 work

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -18,8 +18,8 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1> 
       <div className={classes.value}>{counter}</div>
-      <button onClick={incrementhandler}>incremet</button>
-      <button onClick={decrementhandler}>decrement</button>
+      <button onClick={incrementhandler}>incremet by 5</button>
+      <button onClick={decrementhandler}>decrement by 5</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
